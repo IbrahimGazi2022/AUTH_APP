5d3bd2e7b6dc266b65bcd62d9b9c1e2ed101274d
Ibrahim Gazi <coder.ibrahimgazi@gmail.com>
2024-09-12 21:18:59 +0600

    Hashed password + generate verification token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,35 @@
+import { User } from "../models/auth.model.js";
+
 export const signup = async (req, res) => {
-    res.send("Signup Route");
+    const { name, email, password } = req.body;
+
+    try {
+        if (!name || !email || !password) {
+            throw new Error("All fields are required");
+        }
+
+        const userAlreadyExists = await User.findOne({ email });
+        if (userAlreadyExists) {
+            return res.status(400).json({ success: false, message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword,
+            verificationToken,
+            verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24 hour
+        });
+
+        await user.save();
+        res.status(201).json({ success: true, message: "User created successfully" });
+
+    } catch (error) {
+        res.status(400).json({ success: false, message: error.message });
+    }
 };
 
 export const login = async (req, res) => {
